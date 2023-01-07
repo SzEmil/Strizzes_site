50537c9f2318970ed354f1ac7f7d2258ba3ee0b9
Emil Szymczyk <106548124+SzEmil@users.noreply.github.com>
2023-01-07 22:32:54 +0100

    mods

--- src/js/introduction-modal.js
+++ src/js/introduction-modal.js
@@ -1,14 +1,19 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-introduction-open]"),
-    closeModalBtn: document.querySelector("[data-modal-introduction-close]"),
-    modal: document.querySelector("[data-modal-introduction]"),
-  };
+	const refs = {
+		openModalBtn: document.querySelector('[data-modal-introduction-open]'),
+		closeModalBtn: document.querySelector('[data-modal-introduction-close]'),
+		modal: document.querySelector('[data-modal-introduction]'),
+	};
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+	refs.openModalBtn.addEventListener('click', toggleModal);
+	refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
+	function toggleModal() {
+		refs.modal.classList.toggle('is-hidden');
+	}
 })();
+
+var lightbox = new SimpleLightbox('.article-introduction__img-box .gallery__item', {
+	/* options */
+	enableKeyboard: true,
+});
